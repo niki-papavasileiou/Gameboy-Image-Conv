ab0b18a92db3157369b36cb088d19c6f5afe4666
Niki Papavasileiou <107496316+niki-papavasileiou@users.noreply.github.com>
2023-06-06 09:55:30 +0300

    Create m.py

---
+++ m.py
@@ -0,0 +1,314 @@
+import PySpin
+import matplotlib.pyplot as plt
+import sys
+import keyboard
+from tkinter import filedialog
+
+global continue_recording
+continue_recording = True
+current_offset_horizontal = 0 
+current_offset_vertical = 0
+ax = None 
+
+# Functions for moving lines
+def move_lines_left():
+    global current_offset_horizontal
+    current_offset_horizontal -= 10
+    pass
+
+def move_lines_right():
+    global current_offset_horizontal
+    current_offset_horizontal += 10
+    pass
+
+def move_lines_up():
+    global current_offset_vertical
+    current_offset_vertical -= 10
+    pass
+
+def move_lines_down():
+    global current_offset_vertical
+    current_offset_vertical += 10
+    pass
+
+def handle_close(evt):
+    """
+    This function will close the GUI when close event happens.
+
+    :param evt: Event that occurs when the figure closes.
+    :type evt: Event
+    """
+
+    global continue_recording
+    continue_recording = False
+
+
+def acquire_and_display_images(cam, nodemap, nodemap_tldevice, i, current_offset_horizontal, current_offset_vertical):
+    """
+    This function continuously acquires images from a device and display them in a GUI.
+
+    :param cam: Camera to acquire images from.
+    :param nodemap: Device nodemap.
+    :param nodemap_tldevice: Transport layer device nodemap.
+    :type cam: CameraPtr
+    :type nodemap: INodeMap
+    :type nodemap_tldevice: INodeMap
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    global continue_recording
+    sNodemap = cam.GetTLStreamNodeMap()
+
+    # Change bufferhandling mode to NewestOnly
+    node_bufferhandling_mode = PySpin.CEnumerationPtr(sNodemap.GetNode('StreamBufferHandlingMode'))
+    if not PySpin.IsAvailable(node_bufferhandling_mode) or not PySpin.IsWritable(node_bufferhandling_mode):
+        print('Unable to set stream buffer handling mode.. Aborting...')
+        return False
+
+    # Retrieve entry node from enumeration node
+    node_newestonly = node_bufferhandling_mode.GetEntryByName('NewestOnly')
+    if not PySpin.IsAvailable(node_newestonly) or not PySpin.IsReadable(node_newestonly):
+        print('Unable to set stream buffer handling mode.. Aborting...')
+        return False
+
+    # Retrieve integer value from entry node
+    node_newestonly_mode = node_newestonly.GetValue()
+
+    # Set integer value from entry node as new value of enumeration node
+    node_bufferhandling_mode.SetIntValue(node_newestonly_mode)
+
+    print('*** IMAGE ACQUISITION ***\n')
+    try:
+        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+        if not PySpin.IsAvailable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
+            print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
+            return False
+
+        # Retrieve entry node from enumeration node
+        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+        if not PySpin.IsAvailable(node_acquisition_mode_continuous) or not PySpin.IsReadable(
+                node_acquisition_mode_continuous):
+            print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
+            return False
+
+        # Retrieve integer value from entry node
+        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+
+        # Set integer value from entry node as new value of enumeration node
+        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+        print('Acquisition mode set to continuous...')
+
+        cam.BeginAcquisition()
+
+        print('Acquiring images...')
+
+        device_serial_number = ''
+        node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+        if PySpin.IsAvailable(node_device_serial_number) and PySpin.IsReadable(node_device_serial_number):
+            device_serial_number = node_device_serial_number.GetValue()
+            print('Device serial number retrieved as %s...' % device_serial_number)
+
+        # Close program
+        print('Press enter to close the program..')
+
+        # Figure(1) is default so you can omit this line. Figure(0) will create a new window every time program hits this line
+        fig = plt.figure(1)
+
+        # Close the GUI when close event happens
+        fig.canvas.mpl_connect('close_event', handle_close)
+
+        # Retrieve and display images
+        while(continue_recording):
+            try:               
+                image_result = cam.GetNextImage()
+
+                #  Ensure image completion
+                if image_result.IsIncomplete():
+                    print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+
+                else:                    
+                    # Getting the image data as a numpy array
+                    image_data = image_result.GetNDArray()
+                    center_x = image_data.shape[1] / 2 + current_offset_horizontal
+                    center_y = image_data.shape[0] / 2 + current_offset_vertical
+                    
+                    # Draws an image on the current figure
+                    plt.imshow(image_data, cmap='gray')
+                    plt.text(100,100, 'Camera ' + str(device_serial_number), fontfamily='serif', fontsize=10, color='r')
+                    plt.scatter(center_x, center_y, s=1, color='red', marker = '+', linewidths=320) #Create rhe red cross in the center of the image
+                    # Interval in plt.pause(interval) determines how fast the images are displayed in a GUI
+                    # Interval is in seconds.
+                    plt.pause(0.0001)
+
+                    # Clear current reference of a figure. This will improve display speed significantly
+                    plt.clf()
+                    
+                    print('Camera ' + str(i) + ' gain at ' + str(cam.Gain.GetValue())+'   Camera '+ str(i) + ' exposure time at ' + str(cam.ExposureTime.GetValue()), end='\r')
+
+                    # If user presses enter, close the program
+                    if keyboard.is_pressed('ENTER'):
+                        print('Program is closing...')
+                        
+                        # Close figure
+                        plt.close('all')             
+                        input('Done! Press Enter to exit...')
+                        
+                        continue_recording=False                        
+
+                image_result.Release()
+
+            except PySpin.SpinnakerException as ex:
+                print('Error: %s' % ex)
+                return False
+
+        cam.EndAcquisition()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return True
+
+def capture_ss(cam, current_offset_horizontal,current_offset_vertical):
+    try:
+        image_result = cam.GetNextImage()
+        if image_result.IsIncomplete():
+            print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+            return
+
+        image_data = image_result.GetNDArray()
+        image_result.Release()
+
+        center_x = image_data.shape[1] / 2 + current_offset_horizontal
+        center_y = image_data.shape[0] / 2 + current_offset_vertical
+
+        ax.imshow(image_data, cmap='gray')
+        ax.scatter(center_x, center_y, s=1, color='red', marker='+', linewidths=320)
+
+        plt.pause(0.0001)
+
+        filename = filedialog.asksaveasfilename(defaultextension=".jpg",
+                                                filetypes=[("JPEG Image", "*.jpg"), ("PNG Image", "*.png")])
+        if filename:
+            plt.savefig(filename)
+            print('Snapshot saved as %s' % filename)
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+
+def run_single_camera(cam, i):
+    """
+    This function acts as the body of the example; please see NodeMapInfo example
+    for more in-depth comments on setting up cameras.
+
+    :param cam: Camera to run on.
+    :type cam: CameraPtr
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    try:
+        result = True
+        nodemap_tldevice = cam.GetTLDeviceNodeMap()
+
+        # Initialize camera
+        cam.Init()
+        # Retrieve GenICam nodemap
+        nodemap = cam.GetNodeMap()
+        cam.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+        cam.GainAuto.SetValue(PySpin.GainAuto_Continuous)
+        cam.ExposureAuto.SetValue(PySpin.ExposureAuto_Continuous)
+        
+        import tkinter as tk
+        from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+        root = tk.Tk()
+        
+        fig, ax = plt.subplots()
+        canvas = FigureCanvasTkAgg(fig, master=root)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+        btn_left = tk.Button(root, text='Left', command=lambda:move_lines_left)
+        btn_left.pack(side=tk.LEFT)
+        btn_right = tk.Button(root, text='Right', command=lambda:move_lines_right)
+        btn_right.pack(side=tk.LEFT)
+        btn_up = tk.Button(root, text='Up', command=lambda:move_lines_up)
+        btn_up.pack(side=tk.LEFT)
+        btn_down = tk.Button(root, text='Down', command=lambda :move_lines_down)
+        btn_down.pack(side=tk.LEFT)
+
+        btn_capture = tk.Button(root, text='Capture', command=lambda:capture_ss)
+        btn_capture.pack(side=tk.LEFT)
+        
+        # Acquire images
+        result &= acquire_and_display_images(cam, nodemap, nodemap_tldevice, i)
+
+        # Deinitialize camera
+        cam.DeInit()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        result = False
+
+    return result
+
+def main():
+    """
+    Example entry point; notice the volume of data that the logging event handler
+    prints out on debug despite the fact that very little really happens in this
+    example. Because of this, it may be better to have the logger set to lower
+    level in order to provide a more concise, focused log.
+
+    :return: True if successful, False otherwise.
+    :rtype: bool
+    """
+    result = True
+
+    # Retrieve singleton reference to system object
+    system = PySpin.System.GetInstance()
+
+    # Get current library version
+    version = system.GetLibraryVersion()
+    print('Library version: %d.%d.%d.%d' % (version.major, version.minor, version.type, version.build))
+
+    # Retrieve list of cameras from the system
+    cam_list = system.GetCameras()
+
+    num_cameras = cam_list.GetSize()
+    print('Number of cameras detected: %d' % num_cameras)
+
+    # Finish if there are no cameras
+    if num_cameras == 0:
+        # Clear camera list before releasing system
+        cam_list.Clear()
+
+        # Release system instance
+        system.ReleaseInstance()
+
+        print('Not enough cameras!')
+        input('Done! Press Enter to exit...')
+        return False
+
+    # Run example on each camera
+    for i, cam in enumerate(cam_list):
+
+        print('Running example for camera %d...' % i)
+
+        result &= run_single_camera(cam, i)
+        print('\n Camera %d example complete... \n' % i)
+    del cam
+
+    # Clear camera list before releasing system
+    cam_list.Clear()
+
+    # Release system instance
+    system.ReleaseInstance()
+
+    input('Done! Press Enter to exit...')
+    return result
+
+if __name__ == '__main__':
+    if main():
+        sys.exit(0)
+    else:
+        sys.exit(1)
